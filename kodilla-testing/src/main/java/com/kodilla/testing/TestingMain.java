7d16dae3803b627516317f5a50f2efc8592cabe7
Agata <chmielewska.agata33@gmail.com>
2018-03-13 20:20:26 +0100

    stworzenie klasy Calculaotr, z metodami add i substract oraz test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-       System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+       SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+       String result = simpleUser.getUsername();
+
+       if(result.equals("theForumUser")) {
+           System.out.println("test OK");
+       } else {
+           System.out.println("Error!");
+       }
+
+        Calculator kalkulator = new Calculator();
+        int dodawanie = kalkulator.add(120,4);
+        int odejmowanie = kalkulator.substract(30, 45);
+
+        if(dodawanie == 120+4){
+            System.out.println("dodawanie OK");
+        } else {
+            System.out.println("dodawanie Error!");
+        }
+
+        if(odejmowanie == 30-45){
+            System.out.println("odejmowanie OK");
+        } else {
+            System.out.println("odejmowanie Error!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int a, int b){
+        int result = a+b;
+        return result;
+    }
+
+    public int substract(int a, int b){
+        int result = a-b;
+        return result;
+    }
+}
