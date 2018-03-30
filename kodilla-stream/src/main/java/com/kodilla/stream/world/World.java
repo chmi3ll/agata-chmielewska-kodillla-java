99f6d26f4d8f7ff9dcd087dbac0b0efe82be5c65
Agata <chmielewska.agata33@gmail.com>
2018-03-30 12:09:35 +0200

    problem ze streamem cd

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -5,23 +5,22 @@ import java.util.*;
 
 public final class World {
 
-    final List<Country> continents = new ArrayList<>();
-    public Country addCountry(Continent continent) {
-        continents.add((new BigDecimal(1)), "Afryka");
-        continents.add((new BigDecimal(2)), "Ameryka Polnocna");
-        continents.add((new BigDecimal(3)), "Ameryka Poludniowa");
-    }
-    public List<Country> getContinents(){
-        return new ArrayList<>(continents);
+    final List<Continent> world = new ArrayList<>();
+
+    public void addContinent (Continent continent)  {
+        world.add(continent);
     }
 
+    public List<Continent> getContinents() {
+        return new ArrayList<>(world);
+    }
 
-    final List <Country> world = new ArrayList<>();
-    public List<BigDecimal> getPeopleQuantity(){
-    return world.stream()
-            .map(Country::getPeopleQuantity)
-            .flatMap(w -> w.getCountries().stream())
-            .filter(w -> w.getPeopleQuantity() > 3)
-            .reduce(BigDecimal.ZERO), (sum, current) -> sum = sum.add(current));
-        }
+    public List<BigDecimal> getPeopleQuantity() {
+        return world.stream()
+                .flatMap(w -> w.getContinents().stream())
+                .map(w -> w.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+
+    }
 }
+
