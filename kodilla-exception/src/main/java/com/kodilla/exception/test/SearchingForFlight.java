fa056f09e909c5e62b66a06c9bee70299b4d61d8
Agata <chmielewska.agata33@gmail.com>
2018-04-18 18:08:45 +0200

    8.4 Wiedza o wyjątkach

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SearchingForFlight.java
@@ -0,0 +1,40 @@
+package com.kodilla.exception.test;
+
+import java.util.*;
+
+public class SearchingForFlight {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        String airport = flight.getDepartureAirport();
+        SearchingForFlight searchingForFlight = new SearchingForFlight();
+         Map<String, Boolean> status = searchingForFlight.getAirport();
+        if(status.containsKey(airport)) {
+            System.out.println("route avaiable");
+            } else {
+                 System.out.println(status.get(airport));
+                 throw new RouteNotFoundException("route not found!");
+        }
+    }
+    public Map<String, Boolean> getAirport() {
+        Map<String, Boolean> airportStatus = new HashMap<>();
+        airportStatus.put("Warszawa", true);
+        airportStatus.put("Amsterdam", false);
+        airportStatus.put("Frankfurt", true);
+        airportStatus.put("Portland", true);
+        return airportStatus; }
+
+    public static void main(String[] args) {
+    SearchingForFlight searchingForFlight = new SearchingForFlight();
+
+        try {
+            searchingForFlight.findFlight(new Flight("", "Kraków"));
+
+        } catch (RouteNotFoundException e){
+            System.out.println("route not found");
+        }
+        finally {
+            System.out.println("choose from our 10000 flights");
+        }
+
+    }
+}
