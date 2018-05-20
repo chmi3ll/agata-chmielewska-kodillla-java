88aa758c3beb030137e3e52f677ae5d8f3c641c4
Agata <chmielewska.agata33@gmail.com>
2018-05-20 14:08:50 +0200

    13.2 Tworzymy encje list zadan

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindListName(){
+        //Given
+        TaskList taskList = new TaskList("takaLista", "tasklista do zadania");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+        //Then
+        Assert.assertEquals(1, readTaskList.size());
+        //CleanUp
+        taskListDao.deleteAll();
+    }
+}
