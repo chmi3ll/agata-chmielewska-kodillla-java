748ec3aa64cf25c255805b443391654c34ad5987
Agata <chmielewska.agata33@gmail.com>
2018-05-21 19:31:29 +0200

    13.3 Naglowki faktur oraz ich pozycje - wszystko

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,5 +1,8 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -12,6 +15,8 @@ public class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task(String description, int duration) {
         this.description = description;
@@ -23,7 +28,8 @@ public class Task {
     }
 
     @Id
-    @GeneratedValue
+    @GenericGenerator(name="inc" , strategy="increment")
+    @GeneratedValue(generator="inc")
     @Column (name = "ID", unique = true)
     public int getId() {
         return id;
@@ -45,6 +51,18 @@ public class Task {
         return duration;
     }
 
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "TASKS_FINANCIALS_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "TASKLIST_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -59,5 +77,13 @@ public class Task {
 
     private void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
     }
 }
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+
+        Product product1 = new Product("jabłko");
+        Product product2 = new Product("rzodkiewka");
+        Product product3 = new Product("ser");
+
+        Item item1 = new Item(product1, new BigDecimal("2"), 10);
+        Item item2 = new Item(product2, new BigDecimal("4"), 30);
+        Item item3 = new Item(product3, new BigDecimal("34"), 1);
+
+        Invoice invoice = new Invoice("12345");
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(1, id);
+
+        //CleanUp
+        //taskDao.deleteById(id);
+
+    }
+
+}
